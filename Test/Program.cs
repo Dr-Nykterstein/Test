add85cfef510cc0619561c898404c1e72f8e9bd5
alexandr <ashkurinskiy@gmail.com>
2020-03-31 15:08:08 +0300

    function Draw

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,7 +6,27 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char symb1 = '*';
+            
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(symb1);
+            
+            
+            int x2 = 4;
+            int y2 = 5;
+            char symb2 = '#';
+            
+            Draw(x2, y2, symb2);
+            
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x,y);
+            Console.Write(sym);
         }
     }
 }
