e29c378697e7ce5c8b9c3d0d0766c551f16809ed
alexandr <ashkurinskiy@gmail.com>
2020-03-31 16:03:14 +0300

    list

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Test
 {
@@ -11,6 +12,28 @@ namespace Test
 
             Point p2 = new Point(4, 5, '$');
             p2.Draw();
+            
+            List<int> numList = new List<int>();
+            numList.Add(2);
+            numList.Add(5);
+            numList.Add(10);
+ 
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+            numList.RemoveAt(0);
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            for (int i = 0; i < pList.Count; i++)
+            {
+                Console.WriteLine(pList[i].x);
+            }
             Console.ReadLine();
         }
     }
